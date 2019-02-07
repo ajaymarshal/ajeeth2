fccc7db8d7a08b41d7e25ae4a166f27a155fe549
ajaymarshal <46019554+ajaymarshal@users.noreply.github.com>
2019-02-07 14:29:06 +0530

    Create 12.c

---
+++ 12.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+   int n, reverse = 0, t;
+   printf("Enter a number :\n");
+   scanf("%d", &n);
+   t = n;
+   while (t != 0)
+   {
+      reverse = reverse * 10;
+      reverse = reverse + t%10;
+      t = t/10;
+   }
+   if (n == reverse)
+      printf("yes");
+   else
+      printf("no");
+   return 0;
+}
